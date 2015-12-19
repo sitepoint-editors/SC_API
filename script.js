68de92ed8c2d124e268f4c9737643b76b99a9421
Mustaghees Butt <renaldpirate@gmail.com>
2015-12-19 18:58:19 +0500

    added functionality
    JS, CSS and user interface was setup.

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+	// Initialization of SDK
+	SC.initialize({
+		client_id: "21832d295e3463208d2ed0371ae08791",
+		callback: "mustagheesbutt.github.io/SC_API/callback.html"
+	});
+	
+	// Login handler
+	$("#login").click(function () {
+		SC.connect(function () {
+			SC.get("/me", function (me) {
+				$("main h2 span").html(me.username);
+			});
+			if (SC.isConnected) {
+				$("header, main").addClass("loggedIn");
+			}
+		});
+	});
+	
+});
