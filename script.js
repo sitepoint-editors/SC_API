24b85327b4511a37bc42b91b4c1f4413d188b191
Mustaghees Butt <renaldpirate@gmail.com>
2015-12-20 13:45:31 +0500

    audio now playable
    Tracks and Playlists can now be played

--- script.js
+++ script.js
@@ -19,27 +19,48 @@ $(document).ready(function () {
 		});
 	});
 	
-	// function for user tracks
-	function getTracks() {
-		SC.get("/me/tracks", function (resp) {
-			for (var i=0; i< resp.length; i++) {
-				$("#tracklist ul").append("<li>" + resp[i].title + "</li>")
-			}
-		});
-	}
-	// function for user playlists
-	function getPlaylists() {
-		SC.get("/me/playlists", function (resp) {
-			for (var i=0; i< resp.length; i++) {
-				$("#playlists ul").append("<li>" + resp[i].title + "</li>")
-			}
-		});
-	}
 	// set User Interface
 	function setUI(userName, avatar, description) {
 		$("main h2 span").html(userName);
 		$("#avatar").attr("src", avatar);
 		$("#description").html(description);
 	}
+	// function for user tracks
+	var tracks = {};
+	function getTracks() {
+		SC.get("/me/tracks", function (resp) {
+			for (var i=0; i< resp.length; i++) {
+				$("#tracklist ul").append("<li class='playable'>" + resp[i].title + "</li>");
+				tracks[ resp[i].title ] = resp[i].permalink;
+			}
+		});
+	}
+	// function for user playlists
+	var playlists = {};
+	function getPlaylists() {
+		SC.get("/me/playlists", function (resp) {
+			for (var i=0; i< resp.length; i++) {
+				$("#playlists ul").append("<li class='playable'>" + resp[i].title + "</li>");
+				playlists[ resp[i].title ] = resp[i].permalink;
+			}
+		});
+	}
+	// play something
+	function play(uri) {
+		url = "http://soundcloud.com/" + user_perma + "/" + uri;
+		SC.oEmbed(url, {maxwidth: 200}, function (resp) {
+			$("#player").html(resp.html);
+		});
+	}
+	
+	// when a track or playlist gets clicked, play it using `play()` function
+	$("li.playable").click(function (e) {
+		var title = $(this).html();
+		if ( tracks.hasOwnProperty(title) ) {
+			play(tracks[title]);
+		} else if (playlists.hasOwnProperty(title)) {
+			play("sets/" + playlists[title]);
+		}
+	});
 	
 });
